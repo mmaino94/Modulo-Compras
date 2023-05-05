602c193da5575977e6baf7f5f074c012dbbf434a
mmaino94 <martinmainogranate@hotmail.com>
2023-05-05 16:04:54 -0300

    "Agrega nuevas funciones al archivo main.py y actualiza los datos de proveedores en proveedores.csv"

--- src/app/main.py
+++ src/app/main.py
@@ -5,10 +5,87 @@ from email.mime.multipart import MIMEMultipart
 
 
 def read_files():
-    proveedores = pd.read_csv("./src/data/proveedores.csv")
+    proveedores = pd.read_csv(r"C:\Users\mmaino\Desktop\Python\Modulo-Compras\src\data\proveedores.csv")
     materiales = pd.read_csv("./src/data/materiales1.csv")
 
     return proveedores, materiales
+
+# Creamos un diccionario vacio para asociar cada grupo de materiales con sus proveedores
+def grupos(proveedores):
+    proveedores_grupos = {}
+
+    for mat in proveedores['Material'].unique():
+        #add key and value to dictionary
+        proveedores_grupos[mat] = proveedores[proveedores['Material']==mat]['Mail']
+        
+    # Creamos una lista vacía para los grupos de materiales que no tienen proveedores asociados
+    grupos_sin_proveedor = []
+
+    # Recorrer la tabla de ejemplo de materiales y agregar cada grupo de materiales único a la lista de grupos seleccionados
+
+    grupos_seleccionados = []
+
+    # Defino los grupos disponibles en la lista de materiales
+    
+    grupos_disponibles = materiales['Grupo'].unique().tolist()
+
+
+    # Recorrer la tabla de ejemplo de materiales y agregar cada grupo de materiales único a la lista de grupos seleccionados
+    for grupo in proveedores['Material'].unique():
+        grupos_seleccionados.append(grupo)
+
+    #Generacion de la lista final de grupos
+    grupos_final = list(set(grupos_disponibles).intersection(grupos_seleccionados))
+    grupos_final
+    
+        
+    for grupo in grupos_disponibles:
+        # Verificamos si el grupo tiene proveedor asociado
+        if grupo not in proveedores_grupos:
+            grupos_sin_proveedor.append(grupo)
+            continue
+
+    print(f'Los Grupos que se solicitara cotizacion son: {grupos_final}')
+    print(f'No se encontro proveedor para: {grupos_sin_proveedor}')
+
+    # Preguntar si el usuario desea eliminar algún grupo
+    eliminar = input('¿Desea eliminar algún grupo? (S/N): ')
+
+    if eliminar.lower() == 's':
+        # Solicitar al usuario que ingrese los grupos a eliminar
+        eliminar_grupos = input('Ingrese los grupos que desea eliminar (separados por comas): ').split(',')
+        # Eliminar los espacios en blanco de los nombres de grupo
+        #eliminar_grupos = [grupo.strip() for grupo in eliminar_grupos]
+
+        # Eliminar los grupos seleccionados de la lista de grupos a enviar correo
+        for grupo in eliminar_grupos:
+            if grupo in grupos_final:
+                grupos_final.remove(grupo)
+                print(f'Se ha eliminado el grupo {grupo} de la lista de grupos seleccionados.')
+            else:
+                print(f'El grupo {grupo} no se encontraba en la lista de grupos seleccionados.')
+    else:
+        print('No se eliminará ningún grupo.')
+
+    return grupos_final
+  
+
+#Funcion para crear el mensaje y enviar a los grupos correctos
+def mensaje_final(grupos_seleccionados):
+    mensajes = {}
+
+    for grupo in grupos_seleccionados:
+        materiales_grupo = materiales[materiales['Grupo'] == grupo]
+        materiales_grupo = materiales_grupo[['Material', 'Cantidad']]
+        materiales_grupo = materiales_grupo.reset_index(drop=True)
+        mensajes[grupo] = crear_cuerpo_mensaje(materiales_grupo)
+
+    for grupo in grupos_seleccionados:
+        destinatarios = proveedores_grupos[grupo].tolist()
+        for des in destinatarios:
+            #enviar_correo(des, mensajes[grupo])
+            print("Email enviado a " + des + " con el grupo " + grupo)
+
 
 #create html email body with material and quantities inside a table, the table is created with a for loop from the dataframe
 
@@ -60,7 +137,9 @@ def crear_cuerpo_mensaje(df: pd.DataFrame):
 def enviar_correo(destinatario: str, cuerpo_mensaje: str):
     # Crear el mensaje
     mensaje = MIMEMultipart()
-    mensaje['From'] = 'mmaino@meditecna.com.ar'
+    direccion_correo = input('Ingrese su dirección de correo electrónico: ')
+    contraseña = input('Ingrese su contraseña: ')
+    mensaje['From'] = direccion_correo
     mensaje['To'] = destinatario
     mensaje['Subject'] = 'Solicitud de cotización'
     mensaje.attach(MIMEText(cuerpo_mensaje, 'html'))
@@ -68,8 +147,8 @@ def enviar_correo(destinatario: str, cuerpo_mensaje: str):
     # Enviar el mensaje
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as server:
         server.starttls()
-        direccion_correo = input('Ingrese su dirección de correo electrónico: ')
-        contraseña = input('Ingrese su contraseña: ')
+        direccion_correo = direccion_correo
+        contraseña = contraseña
         try:
             server.login(direccion_correo, contraseña)
         except smtplib.SMTPAuthenticationError:
@@ -87,35 +166,8 @@ if __name__ == "__main__":
     try:
         proveedores, materiales = read_files()
         print("se leyeron bien los archivos")
-        proveedores_grupos = {}
-        for mat in proveedores['Material'].unique():
-            #add key and value to dictionary
-            proveedores_grupos[mat] = proveedores[proveedores['Material']==mat]['Mail']
-        # Creamos una lista vacía para los grupos de materiales que no tienen proveedores asociados
-        grupos_sin_proveedor = []
-
-        # Recorrer la tabla de ejemplo de materiales y agregar cada grupo de materiales único a la lista de grupos seleccionados
-
-        grupos_seleccionados = []
-
-        for grupo in materiales['Grupo'].unique():
-            grupos_seleccionados.append(grupo)
-
-        #creo un email con cada grupo de materiales y sus cantidades
-        mensajes = {}
-
-        for grupo in grupos_seleccionados:
-            materiales_grupo = materiales[materiales['Grupo'] == grupo]
-            materiales_grupo = materiales_grupo[['Material', 'Cantidad']]
-            materiales_grupo = materiales_grupo.reset_index(drop=True)
-            mensajes[grupo] = crear_cuerpo_mensaje(materiales_grupo)
-
-        for grupo in grupos_seleccionados:
-            destinatarios = proveedores_grupos[grupo].tolist()
-            for des in destinatarios:
-                enviar_correo(des, mensajes[grupo])
-                print("Email enviado a " + des + " con el grupo " + grupo)
-    
+        grupos_seleccionados=grupos(proveedores)
+        mensaje_final(grupos_seleccionados) 
 
     except Exception as e:
         print(e)
