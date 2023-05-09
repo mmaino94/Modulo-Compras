04d0718d1d17bc2b19ca3548055b5ab2dee663d5
mmaino94 <martinmainogranate@hotmail.com>
2023-05-09 13:57:01 -0300

    Se arregla lo de ruta absoluta

--- src/app/main.py
+++ src/app/main.py
@@ -6,8 +6,8 @@ import os
 
 
 def read_files():
-    ruta_absoluta_materiales = os.path.abspath(r"C:\Users\nombre_usuario\Desktop\Python\Modulo-Compras\src\data\materiales1.csv")
-    ruta_absoluta_proveedores = os.path.abspath(r"C:\Users\nombre_usuario\Desktop\Python\Modulo-Compras\src\data\proveedores.csv")
+    ruta_absoluta_materiales = os.path.abspath(r"C:\Users\mmaino\Desktop\Python\Modulo-Compras\src\data\materiales1.csv")
+    ruta_absoluta_proveedores = os.path.abspath(r"C:\Users\mmaino\Desktop\Python\Modulo-Compras\src\data\proveedores.csv")
     proveedores = pd.read_csv(ruta_absoluta_proveedores)
     materiales = pd.read_csv(ruta_absoluta_materiales)
 
